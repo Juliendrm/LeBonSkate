6ff3f35c42083a34f5ec4115b0465a046e1dfcaa
Hamza Zbili <hamza.zbili.92@gmail.com>
2022-08-25 12:19:07 +0200

    user and order models added

---
+++ models/Order.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+const User = require("../models/User.model");
+
+const orderModel = new Schema({
+  approved: {
+    type: "boolean",
+    default: false,
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  seller: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  skateBoard: {
+    type: Schema.Types.ObjectId,
+    ref: "Skateboard",
+  },
+  timeStamps: true,
+});
+
+const Order = model("Oder", orderModel);
+
+module.exports = Order;
--- models/User.model.js
+++ models/User.model.js
@@ -1,19 +1,29 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
+const userSchema = new Schema({
+  username: {
+    type: Schema.Types.String,
+    trim: true,
+    unique: true,
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
-);
+  password: String,
+  email: {
+    type: Schema.Types.String,
+    // trim removes spaces if they were included in the request
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    // validate makes sure that the email address is in the correct format
+    validate: [validateEmail, "Please provide a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please provide a valid email address",
+    ],
+  },
+  phoneNumber: Schema.Types.Number,
+  timestamps: true,
+});
 
 const User = model("User", userSchema);
 
