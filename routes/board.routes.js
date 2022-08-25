5a1b626df88d3a3245ff32c779ad061fd57d5f9b
Hamza Zbili <hamza.zbili.92@gmail.com>
2022-08-25 16:08:57 +0200

    added board create and find boards routes

---
+++ routes/board.routes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const Board = require("../models/board.model");
+
+router.post("/", async (req, res, next) => {
+  console.log(`test`);
+  try {
+    const { brand, color, size } = req.body;
+    const newBoard = await Board.create({
+      brand: brand,
+      color: color,
+      size: size,
+    });
+    console.log(newBoard);
+    res.status(201).json(newBoard);
+  } catch {
+    res.status(400);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const boardsFound = await Board.find();
+    res.status(201).json(boardsFound);
+  } catch {
+    res.status(400);
+  }
+});
+
+module.exports = router;
