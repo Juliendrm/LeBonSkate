15231451806bf56f9ea8f7b84b5311938042b1d1
Julien <benedetjulien@gmail.com>
2022-08-26 17:52:11 +0200

    Added the skateboard route

--- models/skateboard.model.js
+++ models/skateboard.model.js
@@ -1,27 +1,26 @@
 const { Schema, model } = require("mongoose");
-const Board = require("../models/board.model"); // importing Board model;
-const Truck = require("../models/truck.model"); // Importing Truck model;
-const Wheels = require("../models/wheels.model"); // Importing Wheels;
+// const Board = require("../models/board.model"); // importing Board model;
+// const Truck = require("../models/trucks.model"); // Importing Truck model;
+// const Wheels = require("../models/wheels.model"); // Importing Wheels;
 
 const skateboardSchema = new Schema({
   board: {
     type: Schema.Types.ObjectId,
     ref: "Board",
   },
-  truck: {
+  trucks: {
     type: Schema.Types.ObjectId,
-    ref: "Truck",
+    ref: "Trucks",
   },
   wheels: {
     type: Schema.Types.ObjectId,
     ref: "Wheels",
   },
-  timestamp: true,
 });
 
 const Skateboard = model("Skateboard", skateboardSchema);
 
 module.exports = Skateboard;
-module.exports = Board;
-module.exports = Truck;
-module.exports = Wheels;
+// module.exports = Board;
+// module.exports = Truck;
+// module.exports = Wheels;
